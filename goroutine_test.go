1c09a4e86ba985e4cb493a90cedae2562c5f73d8
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2023-02-01 20:25:46 +0700

    Add several comments

--- goroutine_test.go
+++ goroutine_test.go
@@ -6,12 +6,25 @@ import (
 	"time"
 )
 
+// Create function using golang goroutine
 func RunHelloWorld() {
 	fmt.Println("Hello World!")
 }
 func TestCreateGoroutine(t *testing.T) {
-	go RunHelloWorld()
+	go RunHelloWorld() // using method "go" in front of the function that was wants to implemetn goroutine
 	fmt.Println("Uups")
 
 	time.Sleep(1 * time.Second)
+}
+
+// Test many goroutine
+func DisplayNumber(number int) {
+	fmt.Println("Number-", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+	time.Sleep(5 * time.Second)
 }
