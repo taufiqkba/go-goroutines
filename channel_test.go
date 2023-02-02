1d50794ebe9151cfd6a0527865a2102dc3c38ace
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2023-02-02 10:26:00 +0700

    Select channel in go

--- channel_test.go
+++ channel_test.go
@@ -100,3 +100,32 @@ func TestRangeChannel(t *testing.T) {
 		fmt.Println("Receive data,", data)
 	}
 }
+
+// Select Channel
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data from Channel 1,", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data from Channel 2,", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+
+}
