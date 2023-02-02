387439921b993712ab6d93ee32482a4650b6d8b5
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2023-02-02 10:31:05 +0700

    Default select channel

--- channel_test.go
+++ channel_test.go
@@ -129,3 +129,33 @@ func TestSelectChannel(t *testing.T) {
 	}
 
 }
+
+// Default Channel
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data from Channel 1,", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data from Channel 2,", data)
+			counter++
+		default:
+			fmt.Println("Waiting data response...")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+
+}
