060208c558e03e12001f83b0385f8c728a5e1a5c
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2023-02-04 17:25:10 +0700

    Learn Wait Group to resolve another case in race condition problems

---
+++ wait_group_test.go
@@ -0,0 +1,26 @@
+package gogoroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func RunAsynchronous(group *sync.WaitGroup) {
+	defer group.Done()
+	group.Add(1)
+
+	fmt.Println("Wait group...")
+	time.Sleep(1 * time.Second)
+}
+
+func TestWaitGroup(t *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go RunAsynchronous(group)
+	}
+	group.Wait()
+	fmt.Println("Its DOne!")
+}
